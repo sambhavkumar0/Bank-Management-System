22616d105c2830d45c60d67956ec2db14c5ce0fe
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2024-03-11 22:29:52 +0530

    Signup3 page added

---
+++ src/bank/management/system/Signup3.java
@@ -0,0 +1,84 @@
+package bank.management.system;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+
+public class Signup3 extends JFrame implements ActionListener {
+    JRadioButton r1,r2,r3,r4;
+
+    JCheckBox c1,c2,c3,c4,c5,c6;
+    JButton s1,c;   //sumbit and cancel
+
+    String formno;
+    Signup3(String formno){
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(150,5,100,100);
+        add(image);
+
+        JLabel l1 = new JLabel("Page 3:");
+        l1.setFont(new Font("Raleway",Font.BOLD,22));
+        l1.setBounds(280,40,400,40);
+        add(l1);
+
+        JLabel l2 = new JLabel("Account Details");
+        l2.setFont(new Font("Raleway",Font.BOLD,22));
+        l2.setBounds(280,70,400,40);
+        add(l2);
+
+        JLabel l3 = new JLabel("Account Type:");
+        l3.setFont(new Font("Raleway",Font.BOLD,18));
+        l3.setBounds(100,140,200,30);
+        add(l3);
+
+        r1 = new JRadioButton("Saving Account");
+        r1.setFont(new Font("Raleway",Font.BOLD,16));
+        r1.setBackground(new Color(215,252,252));
+        r1.setBounds(100,180,150,30);
+        add(r1);
+
+        r2 = new JRadioButton("Fixed Deposite Account");
+        r2.setFont(new Font("Raleway",Font.BOLD,16));
+        r2.setBackground(new Color(215,252,252));
+        r2.setBounds(350,180,300,30);
+        add(r2);
+
+        r3 = new JRadioButton("Curent Account");
+        r3.setFont(new Font("Raleway",Font.BOLD,16));
+        r3.setBackground(new Color(215,252,252));
+        r3.setBounds(100,220,250,30);
+        add(r3);
+
+        r4 = new JRadioButton("Recurring Deposite Account");
+        r4.setFont(new Font("Raleway",Font.BOLD,16));
+        r4.setBackground(new Color(215,252,252));
+        r4.setBounds(350,220,200,30);
+        add(r4);
+
+        JLabel l4 = new JLabel("Card Number:");
+        l4.setFont(new Font("Raleway",Font.BOLD,18));
+        l4.setBounds(100,300,200,30);
+        add(l4);
+        
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(r1);
+        buttonGroup.add(r2);
+        buttonGroup.add(r3);
+        buttonGroup.add(r4);
+    }
+}
